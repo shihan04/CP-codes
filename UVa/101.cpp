2056f24c4c8fea7db16d1a2dbebc5f49df08e41c
shihan04 <47939240+shihan04@users.noreply.github.com>
2024-01-24 17:48:34 +0600

    Update 101.cpp

--- UVa/101.cpp
+++ UVa/101.cpp
@@ -55,10 +55,40 @@ int main()
             cin>>a>>c>>b;
             if(a==b || pos[a]==pos[b]) continue;
             if(c=="onto"){
-                
+                while(st[pos[a]].top()!=a){
+                    p.push(st[pos[a]].top());
+                    st[pos[a]].pop();
+                }
+                while(st[pos[b]].top()!=b){
+                    q.push(st[pos[b]].top());
+                    st[pos[b]].pop();
+                }
+                st[pos[b]].push(st[pos[a]].top());
+                st[pos[a]].pop();
+                while(!p.empty()){
+                    st[pos[b]].push(p.top());
+                    pos[p.top()]=pos[b];
+                    p.pop();
+                }
+                while(!q.empty()){
+                    st[pos[b]].push(q.top());
+                    q.pop();
+                }
+                pos[a]=pos[b];
             }
             if(c=="over"){
-                
+                while(st[pos[a]].top()!=a){
+                    p.push(st[pos[a]].top());
+                    st[pos[a]].pop();
+                }
+                st[pos[b]].push(st[pos[a]].top());
+                st[pos[a]].pop();
+                while(!p.empty()){
+                    st[pos[b]].push(p.top());
+                    pos[p.top()]=pos[b];
+                    p.pop();
+                }
+                pos[a]=pos[b];
             }
         }
     }
