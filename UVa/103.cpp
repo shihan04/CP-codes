812b825a78c6d9a823e6599248c0f5514a2146eb
shihan04 <47939240+shihan04@users.noreply.github.com>
2024-01-25 01:44:21 +0600

    Update 103.cpp

--- UVa/103.cpp
+++ UVa/103.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int k,n,a,p;
     while(cin>>k>>n){
-        vector <vector<int>> v[k];
+        vector <vector<int>> v(k);
         vector <int> ans;
         for(int i=0;i<k;i++){
             for(int j=0;j<n;j++){
@@ -17,7 +17,7 @@ int main()
         sort(v.begin(),v.end());
         ans.push_back(v[0][n]);
         p=0;
-        for(i=1;i<k;i++){
+        for(int i=1;i<k;i++){
             bool c=0;
             for(int j=0;j<n;j++){
                 if(v[p][j]>=v[i][j]){
@@ -29,7 +29,7 @@ int main()
             ans.push_back(v[i][n]);
             p=i;
         }
-        cout<ans.size()<<"\n":
+        cout<<ans.size()<<"\n";
         for(int i=0;i<ans.size();i++){
             cout<<ans[i];
             if(i!=ans.size()-1) cout<<" ";
