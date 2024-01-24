e75cc2d5614490bebe064189f1cbfcf3707b8783
shihan04 <47939240+shihan04@users.noreply.github.com>
2024-01-25 00:06:55 +0600

    Update 101.cpp

--- UVa/101.cpp
+++ UVa/101.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    int n,a,b,t;
+    int n,a,b,t,tmp;
     cin>>n;
     int pos[n];
     stack <int> st[n],p,q;
@@ -16,39 +16,48 @@ int main()
         if(s=="quit") break;
         else if(s=="move"){
             cin>>a>>c>>b;
-            if(a==b || pos[a]==pos[b]) continue;
+            if(a==b || pos[a]==pos[b] || a>n || b>n) continue;
             if(c=="onto"){
                 while(st[pos[a]].top()!=a){
-                    p.push(st[pos[a]].top());
+                    //p.push(st[pos[a]].top());
+                    tmp=st[pos[a]].top();
+                    st[tmp].push(tmp);
                     st[pos[a]].pop();
+                    pos[tmp]=tmp;
                 }
                 while(st[pos[b]].top()!=b){
-                    q.push(st[pos[b]].top());
+                    //q.push(st[pos[b]].top());
+                    tmp=st[pos[b]].top();
+                    st[tmp].push(tmp);
                     st[pos[b]].pop();
+                    pos[tmp]=tmp;
                 }
                 st[pos[b]].push(st[pos[a]].top());
                 st[pos[a]].pop();
-                while(!p.empty()){
+                /*while(!p.empty()){
                     st[pos[a]].push(p.top());
                     p.pop();
                 }
                 while(!q.empty()){
                     st[pos[b]].push(q.top());
                     q.pop();
-                }
+                }*/
                 pos[a]=pos[b];
             }
             if(c=="over"){
                 while(st[pos[a]].top()!=a){
-                    p.push(st[pos[a]].top());
+                    //p.push(st[pos[a]].top());
+                    tmp=st[pos[a]].top();
+                    st[tmp].push(tmp);
                     st[pos[a]].pop();
+                    pos[tmp]=tmp;
                 }
                 st[pos[b]].push(st[pos[a]].top());
                 st[pos[a]].pop();
-                while(!p.empty()){
+                /*while(!p.empty()){
                     st[pos[a]].push(p.top());
                     p.pop();
-                }
+                }*/
                 pos[a]=pos[b];
             }
         }
@@ -61,8 +70,11 @@ int main()
                     st[pos[a]].pop();
                 }
                 while(st[pos[b]].top()!=b){
-                    q.push(st[pos[b]].top());
+                    //q.push(st[pos[b]].top());
+                    st[st[pos[b]].top()].push(st[pos[b]].top());
+                    tmp=st[pos[b]].top();
                     st[pos[b]].pop();
+                    pos[tmp]=tmp;
                 }
                 st[pos[b]].push(st[pos[a]].top());
                 st[pos[a]].pop();
@@ -71,10 +83,10 @@ int main()
                     pos[p.top()]=pos[b];
                     p.pop();
                 }
-                while(!q.empty()){
+                /*while(!q.empty()){
                     st[pos[b]].push(q.top());
                     q.pop();
-                }
+                }*/
                 pos[a]=pos[b];
             }
             if(c=="over"){
