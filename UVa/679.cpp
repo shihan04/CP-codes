f83b6973d3a1aa629652e06cda2ecb00c087adc5
shihan04 <47939240+shihan04@users.noreply.github.com>
2024-01-25 22:39:15 +0600

    Update 679.cpp

--- UVa/679.cpp
+++ UVa/679.cpp
@@ -8,15 +8,16 @@ int main()
     while(l--){
         cin>>D>>I;
         vector <int> v;
-        v.push_back(0);
         n=1<<(D-1);
-        for(int i=0;i<D;i++,n/=2){
+        v.push_back(n);
+        n/=2;
+        for(int i=1;i<D;i++,n/=2){
             len=v.size();
             for(int j=0;j<len;j++){
                 v.push_back(v[j]+n);
             }
         }
-        cout<<v[I]<<"\n";
+        cout<<v[I-1]<<"\n";
     }
     cin>>l;
     return 0;
