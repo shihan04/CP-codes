abeb680870b468eaec61add0564587667844423f
shihan04 <47939240+shihan04@users.noreply.github.com>
2024-04-12 16:02:46 +0600

    Update 706.cpp

--- UVa/706.cpp
+++ UVa/706.cpp
@@ -176,6 +176,29 @@ int main()
                 for(int j=0;j<n;j++) d[2*n+2]+='-';
                 d[2*n+2]+=' ';
             }
+            else if(s[i]=='4'){
+                d[0]+=' ';
+                for(int j=0;j<n;j++) d[0]+=' ';
+                d[0]+=' ';
+                for(int j=1;j<=n;j++){
+                    d[j]+='|';
+                    for(int k=0;k<n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[n+1]+=' ';
+                for(int j=0;j<n;j++) d[n+1]+='-';
+                d[n+1]+=' ';
+                for(int j=n+2,l=0;l<n;l++,j++){
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[2*n+2]+=' ';
+                for(int j=0;j<n;j++) d[2*n+2]+=' ';
+                d[2*n+2]+=' ';
+            }
+            if(i<s.size()-1){
+                for(int j=0;j<2*n+3;j++) d[j]+=' ';
+            }
         }
         for(int i=0;i<2*n+3;i++){
             cout<<d[i]<<"\n";
