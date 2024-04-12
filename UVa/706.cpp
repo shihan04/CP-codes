2a95e0f6b199fae87d750b7ffb836cf6baeabed0
shihan04 <47939240+shihan04@users.noreply.github.com>
2024-04-12 15:02:44 +0600

    Create 706.cpp

---
+++ UVa/706.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    string s;
+    while(cin>>n>>s){
+        if(n==0) break;
+        string d[2*n+3];
+        for(int i=0;i<2*n+3;i++) d[i]="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='0'){
+                d[0]+=' ';
+                for(int j=0;j<n;j++) d[0]+='-';
+                d[0]+=' ';
+                for(int j=1;j<=n;j++){
+                    d[j]+='|';
+                    for(int k=0;k<n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[n+1]+=' ';
+                for(int j=0;j<n;j++) d[n+1]+='-';
+                d[n+1]+=' ';
+                for(int j=n+2,l=0;l<n;l++,j++){
+                    d[j]+='|';
+                    for(int k=0;k<n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[2*n+2]+=' ';
+                for(int j=0;j<n;j++) d[2*n+2]+='-';
+                d[2*n+2]+=' ';
+            }
+        }
+        for(int i=0;i<2*n+3;i++){
+            cout<<d[i]<<"\n";
+        }
+        cout<<"\n";
+    }
+}
