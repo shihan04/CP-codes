5a8c0af0c11cf53b9411e4195b397388bb040dd9
shihan04 <47939240+shihan04@users.noreply.github.com>
2024-04-12 15:48:40 +0600

    Update 706.cpp

--- UVa/706.cpp
+++ UVa/706.cpp
@@ -9,7 +9,7 @@ int main()
         string d[2*n+3];
         for(int i=0;i<2*n+3;i++) d[i]="";
         for(int i=0;i<s.size();i++){
-            if(s[i]=='0'){
+            if(s[i]=='8'){
                 d[0]+=' ';
                 for(int j=0;j<n;j++) d[0]+='-';
                 d[0]+=' ';
@@ -29,6 +29,55 @@ int main()
                 d[2*n+2]+=' ';
                 for(int j=0;j<n;j++) d[2*n+2]+='-';
                 d[2*n+2]+=' ';
+            }
+            else if(s[i]=='0'){
+                d[0]+=' ';
+                for(int j=0;j<n;j++) d[0]+='-';
+                d[0]+=' ';
+                for(int j=1;j<=n;j++){
+                    d[j]+='|';
+                    for(int k=0;k<n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[n+1]+=' ';
+                for(int j=0;j<n;j++) d[n+1]+=' ';
+                d[n+1]+=' ';
+                for(int j=n+2,l=0;l<n;l++,j++){
+                    d[j]+='|';
+                    for(int k=0;k<n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[2*n+2]+=' ';
+                for(int j=0;j<n;j++) d[2*n+2]+='-';
+                d[2*n+2]+=' ';
+            }
+            else if(s[i]=='1'){
+                for(int j=0;j<=n+1;j++) d[0]+=' ';
+                for(int j=1;j<=n;j++){
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                for(int j=0;j<=n+1;j++) d[n+1]+=' ';
+                for(int j=n+2,l=0;l<n;l++,j++){
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                for(int j=0;j<=n+1;j++) d[2*n+2]+=' ';
+            }
+            else if(s[i]=='7'){
+                d[0]+=' ';
+                for(int j=0;j<n;j++) d[0]+='-';
+                d[0]+=' ';
+                for(int j=1;j<=n;j++){
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                for(int j=0;j<=n+1;j++) d[n+1]+=' ';
+                for(int j=n+2,l=0;l<n;l++,j++){
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                for(int j=0;j<=n+1;j++) d[2*n+2]+=' ';
             }
         }
         for(int i=0;i<2*n+3;i++){
