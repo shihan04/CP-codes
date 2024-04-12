263a6f0616a50a0272a95fde2832f87aff9f7e4f
shihan04 <47939240+shihan04@users.noreply.github.com>
2024-04-12 15:57:50 +0600

    Update 706.cpp

--- UVa/706.cpp
+++ UVa/706.cpp
@@ -79,6 +79,103 @@ int main()
                 }
                 for(int j=0;j<=n+1;j++) d[2*n+2]+=' ';
             }
+            else if(s[i]=='6'){
+                d[0]+=' ';
+                for(int j=0;j<n;j++) d[0]+='-';
+                d[0]+=' ';
+                for(int j=1;j<=n;j++){
+                    d[j]+='|';
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                }
+                d[n+1]+=' ';
+                for(int j=0;j<n;j++) d[n+1]+='-';
+                d[n+1]+=' ';
+                for(int j=n+2,l=0;l<n;l++,j++){
+                    d[j]+='|';
+                    for(int k=0;k<n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[2*n+2]+=' ';
+                for(int j=0;j<n;j++) d[2*n+2]+='-';
+                d[2*n+2]+=' ';
+            }
+            else if(s[i]=='9'){
+                d[0]+=' ';
+                for(int j=0;j<n;j++) d[0]+='-';
+                d[0]+=' ';
+                for(int j=1;j<=n;j++){
+                    d[j]+='|';
+                    for(int k=0;k<n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[n+1]+=' ';
+                for(int j=0;j<n;j++) d[n+1]+='-';
+                d[n+1]+=' ';
+                for(int j=n+2,l=0;l<n;l++,j++){
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[2*n+2]+=' ';
+                for(int j=0;j<n;j++) d[2*n+2]+='-';
+                d[2*n+2]+=' ';
+            }
+            else if(s[i]=='5'){
+                d[0]+=' ';
+                for(int j=0;j<n;j++) d[0]+='-';
+                d[0]+=' ';
+                for(int j=1;j<=n;j++){
+                    d[j]+='|';
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                }
+                d[n+1]+=' ';
+                for(int j=0;j<n;j++) d[n+1]+='-';
+                d[n+1]+=' ';
+                for(int j=n+2,l=0;l<n;l++,j++){
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[2*n+2]+=' ';
+                for(int j=0;j<n;j++) d[2*n+2]+='-';
+                d[2*n+2]+=' ';
+            }
+            else if(s[i]=='2'){
+                d[0]+=' ';
+                for(int j=0;j<n;j++) d[0]+='-';
+                d[0]+=' ';
+                for(int j=1;j<=n;j++){
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[n+1]+=' ';
+                for(int j=0;j<n;j++) d[n+1]+='-';
+                d[n+1]+=' ';
+                for(int j=n+2,l=0;l<n;l++,j++){
+                    d[j]+='|';
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                }
+                d[2*n+2]+=' ';
+                for(int j=0;j<n;j++) d[2*n+2]+='-';
+                d[2*n+2]+=' ';
+            }
+            else if(s[i]=='3'){
+                d[0]+=' ';
+                for(int j=0;j<n;j++) d[0]+='-';
+                d[0]+=' ';
+                for(int j=1;j<=n;j++){
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[n+1]+=' ';
+                for(int j=0;j<n;j++) d[n+1]+='-';
+                d[n+1]+=' ';
+                for(int j=n+2,l=0;l<n;l++,j++){
+                    for(int k=0;k<=n;k++) d[j]+=' ';
+                    d[j]+='|';
+                }
+                d[2*n+2]+=' ';
+                for(int j=0;j<n;j++) d[2*n+2]+='-';
+                d[2*n+2]+=' ';
+            }
         }
         for(int i=0;i<2*n+3;i++){
             cout<<d[i]<<"\n";
